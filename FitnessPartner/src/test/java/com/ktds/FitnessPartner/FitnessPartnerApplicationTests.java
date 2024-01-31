002d308691f3dee8bddadf4d0506e6a1f0b0110f
KangHo <tgb02087@naver.com>
2024-01-31 14:21:18 +0900

    create setting

---
+++ FitnessPartner/src/test/java/com/ktds/FitnessPartner/FitnessPartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.FitnessPartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessPartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
