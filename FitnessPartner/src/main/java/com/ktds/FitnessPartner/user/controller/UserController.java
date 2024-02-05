0cd368f575f5439395d72b8e6972ec24a7ffd977
tgb02087 <tgb02087@naver.com>
2024-02-06 00:08:12 +0900

    update : 로그인 함수 테스트

--- FitnessPartner/src/main/java/com/ktds/FitnessPartner/user/controller/UserController.java
+++ FitnessPartner/src/main/java/com/ktds/FitnessPartner/user/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.ktds.FitnessPartner.user.controller;
 
+import com.ktds.FitnessPartner.user.dto.Email;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     @GetMapping("/join")
-    public String join() {
+    public String join_page() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String join(@ModelAttribute Email email) {
+        System.out.println(email.getEmail());
         return "join";
     }
 }
