fde72c354b1b8f373846f33388724dd74ad7aaf1
tgb02087 <tgb02087@naver.com>
2024-02-06 00:08:20 +0900

    update : 로그인 함수 테스트

---
+++ FitnessPartner/src/main/java/com/ktds/FitnessPartner/user/dto/Email.java
@@ -0,0 +1,14 @@
+package com.ktds.FitnessPartner.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Email {
+    private String email;
+}
